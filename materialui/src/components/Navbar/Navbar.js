09541d8c35f13dfb44a31150a2403347dd3dd429
David22zambrano <david.zambrano17@outlook.com>
2022-10-07 15:10:37 -0500

    react-practices Done

---
+++ materialui/src/components/Navbar/Navbar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+import { mainNavbarItems } from "./const/navbarListItems";
+const Navbar = () => {
+  const drawerWidth = 220;
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          backgroundColor: "#101f33",
+          color: "#fff",
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <Divider />
+      <List>
+        {mainNavbarItems.map((text, index) => (
+          <ListItem key={text.id}>
+            <ListItemButton>
+              <ListItemIcon
+                sx={{
+                  color: "#fff",
+                }}
+              >
+                {text.icon}
+              </ListItemIcon>
+              <ListItemText primary={text.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default Navbar;
